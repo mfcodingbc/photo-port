b089dc71f4a55c982443fd859eb8db57bacec2e2
Mitchell Frey <mfcodingbc@gmail.com>
2022-06-14 23:30:41 -0400

    Photos update in gallery, tests added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import About from './components/About';
-import Gallery from './components/Gallery';
-import Nav from './components/Nav';
+import React, { useState } from "react";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Nav from "./components/Nav";
 
 function App() {
   const [categories] = useState([
@@ -22,16 +22,14 @@ function App() {
 
   return (
     <div>
-      <Nav>
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      </Nav>
+      ></Nav>
       <main>
-        <div>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
-        </div>
       </main>
     </div>
   );
