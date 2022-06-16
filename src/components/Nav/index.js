c93a8ea63a837b28ca3bc686ad3d1ea31a145dbf
Mitchell Frey <mfcodingbc@gmail.com>
2022-06-16 18:33:24 -0400

    Contact Form appears/dissappears on click

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected,
   } = props;
 
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
   }, [currentCategory]);
-  
+
   return (
     <header className="flex-row px-1">
       <h2>
@@ -25,24 +27,31 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
+          <li className={`mx-2 ${contactSelected && "navActive"}`}>
             <a
               data-testid="about"
               href="#about"
+              onClick={() => setContactSelected(false)}
             >
               About me
             </a>
           </li>
           <li>
-            <span>Contact</span>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`} key={category.name}>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name &&
+                !contactSelected &&
+                "navActive"
+              }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
